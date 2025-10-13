189d3d6375833c8063f886f1a9cc727275e7c3d2
tabilob-sketch <tabilob@gmail.com>
2025-10-13 04:50:20 -0300

    nuevos path

--- core/urls.py
+++ core/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.dashboard, name="dashboard"),
+
     path("devices/", views.device_list, name="device_list"),
     path("devices/<int:device_id>/", views.device_detail, name="device_detail"),
-    path("", views.dashboard, name="dashboard"),
-    path("measurements/", views.measurement_list, name="measurement_list"),  
-    path("alerts/", views.alert_list, name="alert_list"),   
-    path('login/', views.login_view, name='login'),        
-    path('register/', views.register_view, name='register'),  
+
+    path("measurements/", views.measurement_list, name="measurement_list"),
+    path("alerts/", views.alert_list, name="alert_list"),
     path("alerts/week/", views.alerts_week, name="alerts_week"),
+
     path("login/", views.login_view, name="login"),
     path("register/", views.register_view, name="register"),
     path("password-reset/", views.password_reset_view, name="password_reset"),
-    path("", views.dashboard, name="dashboard"),
 ]
