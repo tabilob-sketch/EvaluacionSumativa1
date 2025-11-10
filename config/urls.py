584119a65d1ebc8af8dc02dccfad2bbe2f4befa4
tabilob-sketch <tabilob@gmail.com>
2025-11-10 09:50:46 -0300

    nuevos fixes

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include   
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),  
-       
+    path("", include("core.urls")),
 ]
 
 handler404 = "core.views.custom_404"
+
